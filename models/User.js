8df4a9a2c404888af31e58284eb111a07414aa52
divyajyotish <digitalnestasia@gmail.com>
2026-04-27 21:04:51 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  subscription: {type: String, default: "free"},
+  premiumExpiry: Date,
+  createdAt: {type: Date, default: Date.now}
+});
+
+export default mongoose.model("User", userSchema);
