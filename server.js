c6f2d7149da162cfffe10e4c58516258169ac1fd
divyajyotish <digitalnestasia@gmail.com>
2026-04-27 21:00:31 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.js";
+import kundliRoutes from "./routes/kundli.js";
+import paymentRoutes from "./routes/payment.js";
+import adminRoutes from "./routes/admin.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/kundli", kundliRoutes);
+app.use("/api/payment", paymentRoutes);
+app.use("/api/admin", adminRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>console.log("Server running on " + PORT));
